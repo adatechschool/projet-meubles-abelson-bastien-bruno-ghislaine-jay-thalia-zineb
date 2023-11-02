11414d3a8fd29fb0c08a7493730915626c746daa
zineb <zineb.laanaya@gmail.com>
2023-11-02 16:14:05 +0100

    ajout du carroussel

--- react-app/src/components/Carroussel.js
+++ react-app/src/components/Carroussel.js
@@ -1,32 +1,61 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import chaise from '../../src/assets/images/chaise.jpg';
+import table_basse from '../../src/assets/images/table_basse.jpg';
+import chaise2 from '../../src/assets/images/chaise2.jpg';
+
+
 
 const Carroussel = () => {
     return (
+        <div id="carrousel" style={{ width:"20%",margin: "0 auto" }}>
+            <Carousel data-bs-theme="dark">
+                <Carousel.Item >
+                    <img
+                        className="d-block w-100"
+                        src={chaise}
+                        alt="First slide"
 
-        <div>
-            <div id="carouselExample" class="carousel slide">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src={"..."} class="d-block w-100" alt="..." />
-                    </div>
-                    <div class="carousel-item">
-                        <img src={"..."} class="d-block w-100" alt="..." />
-                    </div>
-                    <div class="carousel-item">
-                        <img src={"..."} class="d-block w-100" alt="..." />
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
+                    />
+                    <Carousel.Caption>
+                        <h5>First slide label</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={table_basse}
+                        alt="Second slide"
+
+                    />
+                    <Carousel.Caption>
+                        <h5>Second slide label</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={chaise2}
+                        alt="Third slide"
+
+                    />
+                    <Carousel.Caption>
+                        <h5>Third slide label</h5>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
+
+
+
+
     );
+
 };
 
 export default Carroussel;
