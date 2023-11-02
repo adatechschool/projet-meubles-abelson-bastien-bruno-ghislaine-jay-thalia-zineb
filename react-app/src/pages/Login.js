f624ac646a884cbe8d07ab8ca1c46a7701ac5acf
Ghislaine Aybram <ghislaine.aybram@gmail.com>
2023-11-02 15:43:38 +0100

    page login

--- react-app/src/pages/Login.js
+++ react-app/src/pages/Login.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import BarreNav from '../components/BarreNav';
+import LoginForm from '../components/LoginForm';
 
 
 const Login = () => {
     return (
         <div>
             <BarreNav />
-            <h1>Mon compte</h1>  
+            <h1>Connection</h1>
+            <br />
+            <br />
+            <LoginForm />
         </div>
     );
 };
