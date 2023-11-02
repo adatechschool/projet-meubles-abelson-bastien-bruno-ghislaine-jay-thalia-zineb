b2899082cb8c0a9b528057117bc028815f6361c4
Jay <gilbertmougamadou@yahoo.fr>
2023-11-02 11:08:16 +0100

    ajout modules dotenv et mongoose et ajout nouveaux meubles dans la collection

--- express-app/app.js
+++ express-app/app.js
@@ -71,3 +71,61 @@ app.get("/api/meuble", async (req, res) => {
 });
 
 module.exports = app;
+
+// pour lire les variables du fichier .env
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+
+  // création du modèle Meuble (qui va créer automatiquement la collections "meubles" dans la base de données lorsqu'on va créer les différents meubles juste après)
+  const Meuble = mongoose.model('Meuble', {
+    id: Number,
+    name: String,
+    type: String,
+    price: Number,
+    materials: Array,
+    colors: Array,
+    size: String
+  });
+
+  // création des différents meubles
+  const firstMeuble = new Meuble({
+    id: 1,
+    name: "Bureau gamer Sköptuflund",
+    type: "Bureau",
+    price: 299,
+    materials: ["acier", "panneau de particules", "feuille de mélamine"],
+    colors: ["noir"],
+    size: "185x74x146"
+  });
+
+  const secondMeuble = new Meuble({
+    id: 2,
+    name: "Canapé d'angle 5 places Ländskrona",
+    type: "Canapé",
+    price: 1299,
+    materials: ["contreplaqué", "panneau de particules", "bois", "mousse polyuréthane"],
+    colors: ["gris foncé"],
+    size: "241/281x89x78"
+  });
+
+  const thirdMeuble = new Meuble({
+    id: 3,
+    name: "Dressing avec portes coulissantes et mirroir Skytta",
+    type: "Dressing",
+    price: 1017,
+    materials: ["aluminium", "verre", "pin massif", "acier"],
+    colors: ["gris", "blanc"],
+    size: "251x115x240"
+  });
+
+  console.log(firstMeuble, secondMeuble, thirdMeuble);
+  await firstMeuble.save();
+  await secondMeuble.save();
+  await thirdMeuble.save();
+}
