d6c46b0e65998c58f1c9776137b45471e977cc64
Ghislaine Aybram <ghislaine.aybram@gmail.com>
2023-11-03 15:35:28 +0100

    page creation compte

--- react-app/src/pages/Login.js
+++ react-app/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import BarreNav from '../components/BarreNav';
 import LoginForm from '../components/LoginForm';
-
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
     return (
