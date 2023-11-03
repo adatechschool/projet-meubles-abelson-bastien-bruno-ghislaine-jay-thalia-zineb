014112e13ebba54cff97b24c463be004ab648b0b
zineb <zineb.laanaya@gmail.com>
2023-11-03 16:28:34 +0100

    page detail produit

--- react-app/src/components/Carroussel.js
+++ react-app/src/components/Carroussel.js
@@ -9,44 +9,32 @@ import chaise2 from '../../src/assets/images/chaise2.jpg';
 const Carroussel = () => {
     return (
         <div id="carrousel">
-            <Carousel data-bs-theme="dark">
+            <Carousel data-bg-theme="dark">
                 <Carousel.Item >
                     <img
                         className="d-block w-100"
+                        class = "imgSize"
                         src={chaise}
                         alt="First slide"
-
                     />
-                    <Carousel.Caption>
-                        <h5>First slide label</h5>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+                
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
                         src={table_basse}
                         alt="Second slide"
-
+                        class = "imgSize"
                     />
-                    <Carousel.Caption>
-                        <h5>Second slide label</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
                         src={chaise2}
                         alt="Third slide"
-
+                        class = "imgSize"
                     />
-                    <Carousel.Caption>
-                        <h5>Third slide label</h5>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-                    </Carousel.Caption>
+                    
                 </Carousel.Item>
             </Carousel>
         </div>
--- react-app/src/pages/DetailsProduit.js
+++ react-app/src/pages/DetailsProduit.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import Carroussel from '../components/Carroussel';
 import BarreNav from '../components/BarreNav';
-// import BoutonAchat from '../components/BoutonAchat';
+import BoutonAchat from '../components/BoutonAchat';
 
 const DetailsProduit = () => {
     return (
-        <div class="center">
+        <div>
              <BarreNav />
-            <h1 class="text-white bg-dark">DETAILS DU PRODUIT</h1>
+            <div class="display">
             <div className="display" class="float-start">
             <Carroussel />
             </div>
 
-            <div class="float-end">
-                <h1>description</h1>
+            <div>
+            <h1>Fauteuil en bois et cannage</h1>
+            <br/>
+            <p>Détails du produit</p>
+          
+            <p>COULEUR : Brun</p>
+            
+            <p>DIMENSION : L50,8 x l53 x 68cm</p>
+           
+
+            <p>Livraison à domicile, en magasin ou en relais Pickup</p>
+            <p class="green"> EN STOCK</p>
+            
+            <hr/>
+            <p class="text-end"><span>150 , 00 euros</span></p>
+            <BoutonAchat />
+
+
             </div>  
-         </div>
+
+            </div>
+
+            
+
+        </div>
        
     );
 };
