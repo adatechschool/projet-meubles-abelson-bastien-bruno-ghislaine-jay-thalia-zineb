fb5d237a5774970e98846271a218b9749c20ea4c
Ghislaine Aybram <ghislaine.aybram@gmail.com>
2023-11-08 12:37:01 +0100

    affichage accueil

--- react-app/src/components/CarteProduit.js
+++ react-app/src/components/CarteProduit.js
@@ -5,7 +5,7 @@ import Card from 'react-bootstrap/Card';
 
 const CarteProduit = ( {product} ) => {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '18rem', margin: '10px' }}>
     <Card.Img variant="top" img src={`http://localhost:3001/${product.images[0]}`} alt="Photo du produit" style={{ height:'15rem' }} />
     <Card.Body style = {{ backgroundColor: 'white' }} >
       <Card.Title style = {{ backgroundColor: 'white' }} >{product.name}</Card.Title>
